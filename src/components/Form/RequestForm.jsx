ca96f39f69ffe3ab83d3393b0b92ef5c7f5efeb7
linda mejia <linda.mejia@uconn.edu>
2019-06-21 15:40:43 -0700

    First commit

---
+++ src/components/Form/RequestForm.jsx
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import 'react-dates/initialize';
+import { DayPickerSingleDateController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
+import {Alert, Form, Label, Input, Button } from 'reactstrap';
+import style from './RequestForm.module.css';
+
+
+
+
+class RequestForm extends Component {
+
+    state={
+
+        date: moment(),
+        dateDisplay: moment().format("YYYY-MM-DD"),
+        start:"",
+        end:"",
+        // appointment:{},
+        appointmentsArray:[],
+        tryAgain: false,
+        endValid:null,
+        
+    }
+
+    onSubmit= (e)=>{
+        debugger;
+        e.preventDefault();
+
+
+        const startDate= new Date(this.state.dateDisplay + "T" + this.state.start) 
+        const endDate= new Date(this.state.dateDisplay + "T" + this.state.end)
+
+        const appointment={
+            date: this.state.dateDisplay,
+            start: startDate,
+            end: endDate,
+        };
+
+        if (this.state.end >this.state.start) {
+            if (this.checkArray(appointment)){
+                let joined = this.state.appointmentsArray.concat([appointment])
+               
+                let sorted= joined.sort((a,b) => (a.start > b.start) ? 1 : ((b.start > a.start) ? -1 : 0)); 
+    
+                this.setState({ 
+                    appointmentsArray: sorted,
+                    date: moment(),
+                    dateDisplay: moment().format("YYYY-MM-DD"),
+                    start:"",
+                    end:"",
+                    endValid:false
+                })
+            } else{
+               this.setState({tryAgain:true})
+            }
+          } else {
+            this.setState({endValid:true})
+          }
+
+    }
+
+
+    checkArray = (newAppt) => {
+        debugger;
+        let appointmentsArray= this.state.appointmentsArray
+        
+        //case 0: if empty array
+        if (appointmentsArray.length===0){
+            return true;
+        }
+
+        // case 1: check if newAppt is before appointmentsArray[0]
+        if (newAppt.end<=appointmentsArray[0].start){
+            return true;
+        }
+
+        // case 3: check if newAppt is after appointmentArray[-1]
+        if (newAppt.start>=appointmentsArray[appointmentsArray.length-1].end){
+            return true;
+        }
+
+        // case 2: check if newAppt can fit in between two existing 
+        // appointments in appointmentsArray
+
+        for (let i =0; i<appointmentsArray.length-1; i++){
+            if (appointmentsArray[i].end < newAppt.start &&
+                appointmentsArray[i+1].start > newAppt.end){
+                    return true;
+                }
+               
+        }
+
+        // if none of the above are true
+        return false;
+    }
+
+    onChange =(e) =>{
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value});
+     };
+
+
+    render() {
+        return (
+            <div className= "container-fluid">
+                <div className= "col-sm-12 col-md-6">
+               <Form onSubmit={this.onSubmit}>
+               {/* <form> */}
+                <Label> Date: </Label>
+                {/* <input value={this.state.dateDisplay} /> */}
+                <Input value={this.state.dateDisplay} className={style.input}/>
+                <br/>
+                <div className={style.calendar}>
+                <DayPickerSingleDateController
+                //    id="date_input"
+                date={this.state.date} // momentPropTypes.momentObj or null
+                onDateChange={(date) => { this.setState({ date, dateDisplay: date.format("YYYY-MM-DD") }); }}
+                focused={this.state.focused} // PropTypes.bool
+                onFocusChange={({ focused }) => { this.setState({ focused }); }}
+                isOutsideRange={day => moment().diff(day, "days") > 0}
+                /></div>
+                
+                <br/>
+                    <Label>
+                     Start Time:</Label><br/>
+                     <Input type="time" name="start" value={this.state.start} className={style.input}  onChange= {this.onChange} />
+               
+                
+                <br/>
+                    
+                    <Label>
+                     End Time: </Label>  <br/>
+                     <Input type="time" name="end" min={this.state.start} value={this.state.end} className={style.input}  onChange={this.onChange}/>
+
+                      {this.state.endValid?<Alert color="danger">
+                       There is an error in your end time.
+                       </Alert> :
+                       <span/> }
+                     <br/>
+                  
+                        {/* <button type="default" onClick={this.onSubmit}>Submit </button> */}
+                        <Button color="info" size="lg" onClick={this.onSubmit} disabled={!this.state.end} block>Submit</Button>
+                </Form>
+
+                    <br/>
+                {this.state.tryAgain?
+                <Alert color="info">
+                Please enter a different time. You have overlapping appointments.
+              </Alert>: <span/>}
+            </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default RequestForm;
