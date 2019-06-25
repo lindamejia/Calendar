d648e75b53dcd6127f6a7a808ce4a645c789eea0
linda mejia <linda.mejia@uconn.edu>
2019-06-25 14:45:47 -0700

    Removed offline & online functionality

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import NavBar from "./components/NavBar.jsx";
 
 import { Offline, Online } from "react-detect-offline";
 
-const polling = {
-  enabled: true,
-  interval: 2000,
-  timeout: 1000
-};
+// const polling = {
+//   enabled: true,
+//   interval: 2000,
+//   timeout: 1000
+// };
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Online polling={polling}>
-          <NavBar />
-          <RequestForm />
-        </Online>
+        {/* <Online polling={polling}> */}
+        <NavBar />
+        <RequestForm />
+        {/* </Online> */}
         {/* <Offline polling={polling}>
           <h1>YOU ARE OFFLINE</h1>
         </Offline> */}
